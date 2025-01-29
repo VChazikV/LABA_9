c413c603004ca1b48164768de2bceba037c53f6f
Vadim Chazov <vmchazov@edu.hse.ru>
2025-01-29 22:29:20 +0500

    Добавил Конструкторы и свойства

--- LABA_9/Post.cs
+++ LABA_9/Post.cs
@@ -8,5 +8,151 @@ namespace LABA_9
 {
     internal class Post
     {
+        /// <summary>
+        /// Просмотры
+        /// </summary>
+        int numViews;//Кол-во просмотров
+
+        /// <summary>
+        /// Комментарии
+        /// </summary>
+        int numComments;//Кол-во комментариев
+
+        /// <summary>
+        /// Реакции
+        /// </summary>
+        int numReactions;//Кол-во реакций
+
+        static int countOfPosts = 0;//Кол-во постов
+
+
+        /// <summary>
+        /// Свойство для обработки бизнес-правил для кол-ва просмотров на посте
+        /// </summary>
+        public int Views
+        {
+            get => numViews;
+
+            private set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Кол-во просмотров не может быть меньше 0");
+                    numViews = 0;
+                }
+                else
+                {
+                    numViews = value;
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Свойство для обработки бизнес-правил для кол-ва комментариев на посте
+        /// </summary>
+        public int Comments
+        {
+            get => numComments;
+
+            private set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Кол-во комментариев не может быть меньше 0");
+                    numComments = 0;
+                }
+                else
+                {
+                    numComments = value;
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Свойство для обработки бизнес-правил для кол-ва реакций на посте
+        /// </summary>
+        public int Reactions
+        {
+            get => numReactions;
+
+            private set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Кол-во реакций не может быть меньше 0");
+                    numReactions = 0;
+                }
+                else
+                {
+                    numReactions = value;
+                }
+            }
+        }
+
+        public Post(int numViews, int numComments, int numReactions)
+        {
+            Views = numViews;
+            Comments = numComments;
+            Reactions = numReactions;
+            countOfPosts++;
+        }
+        public Post()
+        {
+            Views = 0;
+            Comments = 0;
+            Reactions = 0;
+            countOfPosts++;
+        }
+
+        public Post(Post p)
+        {
+            Views = p.Views;
+            Comments = p.Comments;
+            Reactions= p.Reactions;
+            countOfPosts++;
+        }
+
+        private static string GetWordForm(int countOfItem, string normal, string singlForm, string pluralForm)
+        {
+            string corectForm;
+            countOfItem = Math.Abs(countOfItem) % 100;//countOfItem всегда не отрицательна, но для проверки добавим модуль
+            int lastNumber = countOfItem % 10;
+            if (countOfItem > 10 && countOfItem < 20)
+            {
+                corectForm = pluralForm;
+            }
+            else if (lastNumber >= 2 && lastNumber <= 4)
+            {
+                corectForm = singlForm;
+            }
+            else if (lastNumber == 1)
+            {
+                corectForm = normal;
+            }
+            else
+            {
+                corectForm = pluralForm;
+            }
+            return corectForm;
+        }
+
+        /// <summary>
+        /// Вывод информации об объекте в формате "N просмотров. M комментариев. Z реакций."
+        /// </summary>
+        public void ShowInfoOfPosts()
+        {
+            Console.WriteLine($"{numViews} {GetWordForm(numViews, "просмотр", "просмотра", "просмотров")}.\n" +
+                $"{numComments} {GetWordForm(numComments, "комментарий", "комментария", "комментариев")}.\n" +
+                $"{numReactions} {GetWordForm(numReactions, "реакция", "реакции", "реакций")}.\n");
+        }
+        /// <summary>
+        /// Вывод информации о кол-ве объектов."
+        /// </summary>
+        public static void ShowCountOfPosts()
+        {
+            Console.WriteLine($"Кол-во постов {countOfPosts}\n");
+        }
     }
 }
--- LABA_9/Program.cs
+++ LABA_9/Program.cs
@@ -4,8 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("TEST");
+            //Post Post_1 = new Post(1, 2, 3);
+            //Post_1.Show();
+            //Post Post_2 = new Post(12, 1232, 33456);
+            //Post_2.Show();
+            //Post Post_3 = new Post(14, 267, 39876);
+            //Post_3.Show();
+            //Post Post_4 = new Post(671, 345, 37651);
+            //Post_4.Show();
+            //Post Post_5 = new Post(451, 7652, 67893);
+            //Post_5.Show();
+            Post.ShowCountOfPosts();
+            Post Post_1 = new Post(3, 2, 3);
+            Post_1.ShowInfoOfPosts();
+            Post.ShowCountOfPosts();
+            Post Post_2 = new Post();
+            Post_2.ShowInfoOfPosts();
+            Post.ShowCountOfPosts();
+            Post Post_3 = new Post(Post_1);
+            Post_3.ShowInfoOfPosts();
+            Post.ShowCountOfPosts();
+            
         }
     }
 }
